2042a1c8c1f5d5c5123bb916831fd8b1e577cd90
thanwin84 <thanwinhline84@gmail.com>
2025-01-30 02:53:18 +0600

    Add Ecommerce project

--- src/pages/thanwin/ThanWinProjectPage.jsx
+++ src/pages/thanwin/ThanWinProjectPage.jsx
@@ -1,17 +1,25 @@
 import { useParams } from 'react-router';
+<<<<<<< HEAD
 import {
   Counter,
   NumberComparator,
   GradeCalculator,
 } from '@/components/thanwin';
+=======
+import { Counter, Ecommerce } from '@/components/thanwin';
+>>>>>>> 93464d1 (Add Ecommerce project)
 
 export const ThanWinProjectPage = () => {
   const { projectId } = useParams();
 
   const componentMap = {
     'counter-app': <Counter />,
+<<<<<<< HEAD
     'grade-calculator': <GradeCalculator />,
     'number-comparator': <NumberComparator />,
+=======
+    'simple-ecommerce': <Ecommerce />,
+>>>>>>> 93464d1 (Add Ecommerce project)
   };
 
   const selectedProject = componentMap[projectId];
@@ -20,5 +28,5 @@ export const ThanWinProjectPage = () => {
     return <div>Project not found</div>;
   }
 
-  return <div className="bg-white px-2 py-2 w-8/12">{selectedProject}</div>;
+  return <div className="bg-white  w-8/12">{selectedProject}</div>;
 };
