4fa27e866d91eba00e56f2958724ac371db08377
thanwin84 <thanwinhline84@gmail.com>
2025-02-14 03:06:47 +0600

    update

--- src/pages/thanwin/ThanWinProjectPage.jsx
+++ src/pages/thanwin/ThanWinProjectPage.jsx
@@ -1,25 +1,19 @@
 import { useParams } from 'react-router';
-<<<<<<< HEAD
 import {
   Counter,
   NumberComparator,
   GradeCalculator,
+  Ecommerce,
 } from '@/components/thanwin';
-=======
-import { Counter, Ecommerce } from '@/components/thanwin';
->>>>>>> 93464d1 (Add Ecommerce project)
 
 export const ThanWinProjectPage = () => {
   const { projectId } = useParams();
 
   const componentMap = {
     'counter-app': <Counter />,
-<<<<<<< HEAD
     'grade-calculator': <GradeCalculator />,
     'number-comparator': <NumberComparator />,
-=======
     'simple-ecommerce': <Ecommerce />,
->>>>>>> 93464d1 (Add Ecommerce project)
   };
 
   const selectedProject = componentMap[projectId];
