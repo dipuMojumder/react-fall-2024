b0faa6fad46dbdac66c8b7a15b20a390ec855926
Armstrong104 <ameertalha22@gmail.com>
2025-02-16 19:36:54 +0600

    Fix sidebar home navigation problem

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router';
 
 import { projects } from '@/data';
+import { appConfig } from '@/config';
 
 export const Sidebar = () => {
   return (
     <nav className="flex flex-col gap-4">
-      <NavLink to="/">Home</NavLink>
+      <NavLink to={`${appConfig.BASE_ROUTE}/`}>Home</NavLink>
       {projects.map((project) => (
         <NavLink key={project.id} to={`projects/${project.id}`}>
           {project.name}
